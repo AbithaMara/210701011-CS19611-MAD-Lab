c6ae230ac9ccc9f204a1288f66ae1347aaba04ed
AbithaMara <210701011@rajalakshmi.edu.in>
2024-05-17 22:19:33 +0530

    Create DBContract.kt

---
+++ EX-05/DBContract.kt
@@ -0,0 +1,11 @@
+object DBContract {
+    const val DATABASE_NAME = "student.db"
+    const val DATABASE_VERSION = 1
+    
+
+    const val TABLE_STUDENT = "Student"
+
+    const val COLUMN_REGISTER_NUMBER = "RegisterNumber"
+    const val COLUMN_NAME = "Name"
+    const val COLUMN_CGPA = "CGPA"
+}
